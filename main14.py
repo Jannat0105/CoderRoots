5bb0c6d8c51a53701fd05050de2205c02b3156e8
Jannat0105 <jennydogra77@gmail.com>
2025-06-18 16:20:46 +0530

    changes

--- main14.py
+++ main14.py
@@ -6,16 +6,37 @@ str4="year"
 print(str1[:2] + str2[-2:])
 print(str3[:2] + str4[-2:])
 
+# question 2 
+
+st1="coder"
+st2="roots"
+st3=st1.replace("c","r")
+st4=st2.replace("r","c")
+print(st3 + " " + st4)
+
+
 
 #question 3
-str1="abc"
-str2="ing"
-str3="ly"
+str5="abc"
+str6="ing"
+str7="ly"
 a=len(str1)
 if a>=3:
-    if (str1.endswith("ing")):
-            print(str1+str3)
+    if (str5.endswith("ing")):
+            print(str5+str7)
     else:
-         print(str1+str2)
+         print(str5+str6)
 else:
-      print(str1)
+      print(str5)
+
+# question 4
+
+s = "hello"
+n = 2
+
+if n < 0 or n >= len(s):
+    print("Index out of range")
+else:
+    s = s[:n] + s[n+1:]
+    print("Result:", s)
+
