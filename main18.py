cc8bc4606f60e2ade75ec83a0fa44b4d12b61a8f
Jannat0105 <jennydogra77@gmail.com>
2025-06-18 13:21:45 +0530

    Add files via upload

---
+++ main18.py
@@ -0,0 +1,30 @@
+# question 1
+t=(12,12,45,"hello", False)
+a=set(t)
+print(a)
+b=tuple(a)
+print(b)
+
+
+#question 2
+t= [[1, 2], [3, 4], [5, 6]]
+f=[]
+for i in t:
+    for j in i:
+        f.append(j)
+print(f)
+
+
+#question 3
+t=(3,5,1,8,2)
+p=max(t)
+k=min(t)
+print("maximum value is: ", p , "minimum value is: ", k)
+
+#question 4
+u=[i for i in range(1,6)]
+print(u)
+v=[i*i*i for i in u]
+print(v)
+res=list(zip(u,v))
+print(res)
